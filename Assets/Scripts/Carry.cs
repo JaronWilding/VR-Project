362f4542e8af3d534076f57265d32b50c594c2c5
Jaron Wilding <jaronwilding95@gmail.com>
2019-11-04 13:08:51 +0800

    Useless

---
+++ Assets/Scripts/Carry.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Utility;
+using HTC.UnityPlugin.Vive;
+
+public class Carry : MonoBehaviour
+{
+    public Transform obj;
+    private ViveRoleSetter setter;
+    private Rigidbody rbd;
+    public ControllerButton button = ControllerButton.Trigger;
+    private bool locked = true;
+
+    void Start()
+    {
+        setter = GetComponent<ViveRoleSetter>();
+        rbd = GetComponent<Rigidbody>();
+        
+        obj.position = transform.position;
+    }
+    void Update()
+    {
+        if(ViveInput.GetPress(setter.viveRole, button) == true)
+        {
+             Destroy(obj.GetComponent<FixedJoint>());
+             obj.GetComponent<Rigidbody>().velocity = rbd.velocity;
+        }
+    }
+    void LetGo()
+    {
+        obj.GetComponent<Rigidbody>().isKinematic = false;
+        obj.parent = null;
+        locked = false;
+    }
+}
---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.PoseTracker;
+using HTC.UnityPlugin.Utility;
+using HTC.UnityPlugin.Vive;
+
+public class Throw : MonoBehaviour
+{
+    public void LetGo()
+    {
+
+    }
+}
